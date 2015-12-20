3a9ec00d8afba08e709acc864a75680cdc152879
Charles Lee <LEEJNC@AU.AMP.LOCAL>
2015-12-20 11:01:04 +1100

    final file

--- run_analysis.R
+++ run_analysis.R
@@ -22,6 +22,10 @@ setwd("C:/Users/LEEJNC/Google Drive/Coursera/getting_cleaning_data/project")
 getwd()
 
 
+#load required libraries
+library(data.table)
+library(dplyr)
+
 ################ Read in Data #######################
 
 #read in training data
@@ -41,6 +45,11 @@ testlabel <- read.table("../UCI HAR Dataset/test/y_test.txt")
 features <- read.table("../UCI HAR Dataset/features.txt")
 activities <- read.table("../UCI HAR Dataset/activity_labels.txt")
 
+
+#read in subjects ranging from 1 to 30
+trainsubjects <- read.table("../UCI HAR Dataset/train/subject_train.txt")
+testsubjects <- read.table("../UCI HAR Dataset/test/subject_test.txt")
+
 #####################################################
 
 ########### Data preparation#########################
@@ -49,7 +58,49 @@ activities <- read.table("../UCI HAR Dataset/activity_labels.txt")
 names(traindata) <- features$V2
 names(testdata) <- features$V2
 
-#add label column to the training and test data
-traindata$label <- trainlabel$V1
-testdata$label <- testlabel$V1
+
+#extract mean and standard deviation columns from each of the dataset
+#did not extract meanfreq as these are not the same as mean
+selectedcolumns <- grepl("mean\\(\\)|std\\(\\)", features$V2)
+trainset <- traindata[, selectedcolumns]
+testset <- testdata[, selectedcolumns]
+
+
+#add activity label and activity name columns to the training and test data
+trainlabel$activity_name = activities[trainlabel$V1, 2]
+testlabel$activity_name = activities[testlabel$V1, 2]
+
+trainset$activity_label <- trainlabel$V1
+testset$activity_label <- testlabel$V1
+
+trainset$activity_name <- trainlabel$activity_name
+testset$activity_name <- testlabel$activity_name
+
+
+#add subjects to the training and test data
+trainset$subject <- trainsubjects$V1
+testset$subject <- testsubjects$V1
+
+
+#move linked columns to the front (and remove activity label as no longer needed)
+trainset <- cbind("activity_name" = trainset$activity_name, "subject" = trainset$subject, traindata[,selectedcolumns])
+testset <- cbind("activity_name" = testset$activity_name, "subject" = testset$subject, testdata[,selectedcolumns])
+
+#combine the training and test data
+fulldata <- rbind(trainset, testset)
+
+###########################################################
+
+
+#############Tidy subset data creation####################
+
+#Create a second, independent tidy data set with the average of each variable for each activity and each subject
+data_subset <- aggregate(fulldata[, 3:ncol(fulldata)], by=list("subject" = fulldata$subject, "activity_name" = fulldata$activity_name), mean)
+
+##########################################################
+
+
+############## Write to txt file for submission ##########
+
+write.table(data_subset, file = "tidy_data.txt", row.names = FALSE)
 
